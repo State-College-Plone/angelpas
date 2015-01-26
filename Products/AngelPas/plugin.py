bfb9b087b84a670bd607fb280d665874fbfd113a
cqp5087 <cqp5087@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2015-01-26 16:47:20 +0000

    ensure that the sections variable in _config is a persistent list

--- Products/AngelPas/plugin.py
+++ Products/AngelPas/plugin.py
@@ -3,6 +3,7 @@ from time import time
 from urllib import urlopen, urlencode
 
 from persistent.dict import PersistentDict
+from persistent.list import PersistentList
 from elementtree import ElementTree
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
@@ -38,15 +39,15 @@ class AngelDataError(Exception):
 class MultiPlugin(BasePlugin):
     security = ClassSecurityInfo()
     meta_type = 'AngelPas Plugin'
-        
+
     ## PAS interface implementations: ############################
-    
+
     # IGroupEnumerationPlugin:
     security.declarePrivate('enumerateGroups')
     def enumerateGroups(self, id=None, title=None, exact_match=False, sort_by=None, max_results=None, **kw):
         # Though title is not (that I know of) guaranteed to be unique or immutable, it probably will be for the set of groups brought into a Plone site. Only by using the title as the group ID can we get Plone to show titles in the Users & Groups control panel's Groups tab, and we have to do that, as the IDs are totally unfriendly. Code for treating ID and title separately is in r9501.
         group_ids = []
-        
+
         # Build list of group IDs we should return:
         if exact_match:  # Should this be case-sensitive?
             title_or_id = title or id
@@ -61,24 +62,24 @@ class MultiPlugin(BasePlugin):
                 each_group_lower = each_group.lower()
                 if (id is None and title is None) or (id is not None and id_lower in each_group_lower) or (title is not None and title_lower in each_group_lower):
                     group_ids.append(each_group)
-        
+
         # For each gathered group ID, flesh out a group info record:
         plugin_id = self.getId()
         group_infos = [{'id': gid, 'pluginid': plugin_id} for gid in group_ids]
-        
+
         # Sort. (We always sort. We explicitly support sorting by ID, but nothing says we can't sort on it when it's not requested, too. This makes the Groups tab of the Users & Groups control panel look nicer.)
         group_infos.sort(key=lambda x: x['id'])
-        
+
         # Truncate, if requested:
         if max_results is not None:
             del group_infos[max_results:]
-        
+
         return tuple(group_infos)
-    
+
     # IUserEnumerationPlugin (so IGroupIntrospection's methods will actually return users):
     def enumerateUsers(self, id=None, login=None, exact_match=False, sort_by=None, max_results=None, fullname=None, **kw):
         user_ids = set()  # tuples of (user ID, login)
-        
+
         # Build list of user IDs we should return:
         if exact_match:  # Should this be case-sensitive?
             if login:
@@ -103,21 +104,21 @@ class MultiPlugin(BasePlugin):
                    (login is not None and login_lower in k_lower) or \
                    (fullname is not None and fullname_lower in user_info['fullname'].lower()):
                     user_ids.add((k, k))
-        
+
         # For each gathered user ID, flesh out a user info record:
         plugin_id = self.getId()
         user_infos = [{'id': x, 'login': y, 'pluginid': plugin_id} for (x, y) in user_ids]
-        
+
         # Sort, if requested:
         if sort_by in ['id', 'login']:
             user_infos.sort(key=lambda x: x[sort_by])
-        
+
         # Truncate, if requested:
         if max_results is not None:
             del user_infos[max_results:]
-        
+
         return tuple(user_infos)
-    
+
     security.declarePrivate('_verifyUserElsewhere')
     def _verifyUserElsewhere(self, login):
         """Like PAS's _verifyUser, turn a login into an info dict. However, look only in other enumerator plugins, skipping ours so we don't infinitely recurse."""
@@ -147,12 +148,12 @@ class MultiPlugin(BasePlugin):
                     msg = 'UserEnumerationPlugin %s error' % enumerator_id
                     logger.debug(msg, exc_info=True)
         return None
-    
+
     # IGroupsPlugin:
     security.declarePrivate('getGroupsForPrincipal')
     def getGroupsForPrincipal(self, principal, request=None):
         return tuple(self._users.get(principal.getId(), {}).get('groups', set()))
-    
+
     # IGroupIntrospection:
     # Ripping the next 2 methods off from Products.PlonePAS.plugins.group.GroupManager because Python 2.6 insists that the self passed into unbound methods has the same class as the class they came from. That's not the problem I want to solve today, so this is a temporary kludge.
     security.declarePrivate('_createGroup')
@@ -198,7 +199,7 @@ class MultiPlugin(BasePlugin):
         group._addRoles(['Authenticated'])
 
         return group.__of__(self)
-    
+
     def getGroupById(self, group_id, default=None):
         if group_id in self._groups:
             plugins = self._getPAS()._getOb('plugins')
@@ -216,12 +217,12 @@ class MultiPlugin(BasePlugin):
         """Return a list of usernames of the members of the group."""
         # As of Plone 3.3rc4, roles assigned to supergroups don't filter down to the members of subgroups, so we cannot nest groups to good effect.
         return [id for (id, info) in self._users.iteritems() if group_id in info['groups']]  # TODO: don't linear scan over users
-    
+
     # IPropertiesPlugin:
     def getPropertiesForUser(self, user, request=None):
         login = user.getUserName()
         is_group = getattr(user, 'isGroup', lambda: None)()
-        
+
         if is_group:
             if login in self._groups:
                 return {'title': login}  # title == id == login. Yuck. See comments under enumerateGroups().
@@ -234,39 +235,39 @@ class MultiPlugin(BasePlugin):
                     ret['email'] = '%s@%s' % (login, email_domain)
                 return ret
         return {}
-    
+
     ## Helper methods: ######################
-    
+
     @property
     def _users(self):
         """Return a mapping where the keys are Access Account IDs (considered equivalent to login names) and the values are user info records.
-        
+
         Example:
-            
+
             {'fsmith':
                 {'groups': set(['Edison Services Demo Course', 'Some Other Demo Course']),
                  'fullname': 'Francis Wimbledoofer Smith'  # optional
                 }
             }
-        
+
         """
         return self._angel_data[0]
-    
+
     @property
     def _groups(self):
         """Return a set of group titles (which are used as group IDs).
-        
+
         The logical group hierarchy isn't exposed to PAS, since roles assigned to supergroups don't filter down to the members of subgroups as of Plone 3.3rc4. Instead, we flatten everything out, telling PAS that a member of a course section's Instructors group is also a member of the course section's general group, for example.
-        
+
         Example:
-            
+
             set(['Philsophy 101 Section 1',
                  'Philsophy 101 Section 1: Team A',
                  'Philsophy 101 Section 1: Instructors'])
-        
+
         """
         return self._angel_data[1]
-    
+
     security.declarePrivate('_roster_xml')
     def _roster_xml(self, section_id):
         """Return the raw XML of the given section, raising an AngelDataError on error."""
@@ -288,7 +289,7 @@ class MultiPlugin(BasePlugin):
             if f:
                 f.close()
         return xml
-    
+
     security.declarePrivate('_roster_tree')
     def _roster_tree(self, xml):
         """Return the parsed XML tree given raw XML from an ANGEL API call, raising an AngelDataError on error."""
@@ -297,7 +298,7 @@ class MultiPlugin(BasePlugin):
             tree = ElementTree.fromstring(xml)
         except ExpatError:
             raise AngelDataError('The roster request returned a non-XML response:\n%s' % xml)
-        
+
         # Test for API-level success:
         success_node = tree.find('./success')
         if not success_node:
@@ -306,25 +307,25 @@ class MultiPlugin(BasePlugin):
                 raise AngelDataError('ANGEL roster request returned an error: %s' % error)
             else:
                 raise AngelDataError('ANGEL roster request failed but returned no discernable error message. Is the API URL correct?')
-        
+
         return tree
-    
+
     @property
     def _angel_data(self):
         """Return the (possibly cached) user and group info from ANGEL as a 2-item tuple: (users, groups).
-        
+
         See _users() and _groups() docstrings for details of each.
-        
+
         On failure, return empty info (so at least people can log into the site using other PAS plugins) and log the failure at level ERROR.
-        
+
         """
-        
+
         @ram.cache(lambda *args: (self._config['url'], self._config['sections'], time() // (24 * 60 * 60)))
         def get_data():
             """Return the user and group info from ANGEL as a 2-item tuple: (users, groups).
-            
+
             Throw AngelDataError on failure.
-            
+
             """
             users = {}
             groups = set()
@@ -338,7 +339,7 @@ class MultiPlugin(BasePlugin):
                 except AngelDataError, e:
                     # but here...
                     # don't throw out the whole tree for one bad section id.
-                    # we will still raise AngelDataError if there is an 
+                    # we will still raise AngelDataError if there is an
                     # IOError connecting, but just ignore bad section ids
                     logger.error('AngelPas plugin got bad xml data for section '
                                  '%s.  Error was: %s' % (s, e.msg))
@@ -347,7 +348,7 @@ class MultiPlugin(BasePlugin):
                 # Add to groups:
                 section_title = tree.findtext('.//roster/course_title')
                 groups.add(section_title)
-                
+
                 for member in tree.getiterator('member'):
                     # Add this group to the member's user info record, also filling out member info like full name as we go:
                     user_id = member.findtext('user_id').lower()
@@ -358,7 +359,7 @@ class MultiPlugin(BasePlugin):
                     u['groups'].add(section_title)
                     if not u['fullname']:
                         u['fullname'] = fullname
-                    
+
                     # Put the person in the Instructors, Writers, or Students groups if appropriate:
                     rights = member.findtext('course_rights')  # They're not admitting these are bit fields, so we won't treat them as such.
                     addendum = _rights_map.get(rights)
@@ -368,16 +369,16 @@ class MultiPlugin(BasePlugin):
                         groups.add(group_title)
                         ## Then, note the user belongs to this group:
                         users[user_id]['groups'].add(group_title)
-                    
+
                     # Put the person in the right team groups:
                     for team in member.getiterator('team'):
                         if team.text:
                             group_title = '%s: %s' % (section_title, team.text)
                             groups.add(group_title)
                             users[user_id]['groups'].add(group_title)
-            
+
             return users, groups
-        
+
         try:
             users, groups = get_data()
         except AngelDataError, e:
@@ -386,26 +387,32 @@ class MultiPlugin(BasePlugin):
         return users, groups
 
     ## ZMI crap: ############################
-    
+
     def __init__(self, id, title=None):
         BasePlugin.__init__(self)
         self._setId(id)
         self.title = title
-        self._config = PersistentDict({'url': 'https://cmsdev1.ais.psu.edu/api/default.asp', 'username': '', 'password': '', 'sections': ['SP200809-UP-ADMIN-Kolbe_Test-001'], 'email_domain': 'psu.edu'})
-    
+        self._config = PersistentDict({
+            'url': 'https://cmsdev1.ais.psu.edu/api/default.asp',
+            'username': '',
+            'password': '',
+            'sections': PersistentList(['SP200809-UP-ADMIN-Kolbe_Test-001']),
+            'email_domain': 'psu.edu',
+        })
+
     # A method to return the configuration page:
     security.declareProtected(ManageUsers, 'manage_config')
     manage_config = PageTemplateFile('config.pt', www_directory)
-    
+
     # Add a tab that calls that method:
     manage_options = ({'label': 'Options',
                        'action': 'manage_config'},) + BasePlugin.manage_options
-    
+
     security.declareProtected(ManageUsers, 'config_for_view')
     def config_for_view(self):
         """Return a mapping of my configuration values, for use in a page template."""
         return dict(self._config)
-    
+
     security.declareProtected(ManageUsers, 'manage_changeConfig')
     def manage_changeConfig(self, REQUEST=None):
         """Update my configuration based on form data."""
